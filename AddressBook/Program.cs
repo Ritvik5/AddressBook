43ddfb67eeb642570b35c1679886c93b6f84e02d
Ritvik5 <sharma.ritvik56@gmail.com>
2023-02-28 01:27:43 +0530

    Contact details of a person

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace AddressBook
+﻿using System.Text.Json;
+using System;
+
+namespace AddressBook
 {
-    internal class Program
+     class Program
     {
-        static void Main(string[] args)
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program");
+            Contact newContact = new Contact();
+
+            newContact.FirstName = "Ritvik";
+            newContact.LastName = "Sharma";
+            newContact.Address = "123 Xyz";
+            newContact.City = "Banglore";
+            newContact.State = "Karnataka";
+            newContact.PostalCode = "12345";
+            newContact.PhoneNumber = "1234567890";
+            newContact.Email = "xyz@gmail.com";
+
+            Console.WriteLine("Contact Deatils :\n");
+            Console.WriteLine($"Name : {newContact.FirstName} {newContact.LastName}");
+            Console.WriteLine("Address : " + newContact.Address);
+            Console.WriteLine("City : " +newContact.City);
+            Console.WriteLine("State : " +newContact.State);
+            Console.WriteLine("PostalCode : " +newContact.PostalCode);
+            Console.WriteLine("PhoneNumber : " +newContact.PhoneNumber);
+            Console.WriteLine("Email : " + newContact.Email);
+
         }
     }
 }
