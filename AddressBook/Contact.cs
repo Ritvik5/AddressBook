82b466f03edc96d31e517970c645662658dd916b
Ritvik5 <sharma.ritvik56@gmail.com>
2023-02-28 16:49:01 +0530

    Taking details from user and Printing it.

--- AddressBook/Contact.cs
+++ AddressBook/Contact.cs
@@ -17,6 +17,60 @@ namespace AddressBook
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
 
+        public static Contact[] ContactList = new Contact[10];
+        public static int ContactCount = 0;
 
+        public void GetContactInfoFromUser()
+        {
+            Console.WriteLine("Enter Contact Information :");
+
+            Console.WriteLine("FirstName :");
+            FirstName = Console.ReadLine();
+
+            Console.WriteLine("LastName :");
+            LastName = Console.ReadLine();
+
+            Console.WriteLine("Address :");
+            Address = Console.ReadLine();
+
+            Console.WriteLine("City :");
+            City = Console.ReadLine();
+
+            Console.WriteLine("State :");
+            State = Console.ReadLine();
+
+            Console.WriteLine("PostalCode :");
+            PostalCode = Console.ReadLine();
+
+            Console.WriteLine("PhoneNumber :");
+            PhoneNumber = Console.ReadLine();
+
+            Console.WriteLine("Email :");
+            Email = Console.ReadLine();
+        }
+
+        public static void AddContactToList(Contact newcontact)
+        {
+            ContactList[ContactCount] = newcontact;
+            ContactCount++;
+        }
+
+        public static void PrintContactList() 
+        {
+            Console.WriteLine("Contact Details :");
+
+            for (int i = 0; i < ContactCount; i++)
+            {
+                Console.WriteLine($"Name        = {ContactList[i].FirstName} {ContactList[i].LastName} ");
+                Console.WriteLine($"Address     = {ContactList[i].Address} ");
+                Console.WriteLine($"City        = {ContactList[i].City} ");
+                Console.WriteLine($"State       = {ContactList[i].State} ");
+                Console.WriteLine($"PostalCode  = {ContactList[i].PostalCode} ");
+                Console.WriteLine($"PhoneNumber = {ContactList[i].PhoneNumber} ");
+                Console.WriteLine($"Email       = {ContactList[i].Email} ");
+                Console.ReadKey();
+            }
+                
+        }
     }
 }
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -9,25 +9,10 @@ namespace AddressBook
         public static void Main(string[] args)
         {
             Contact newContact = new Contact();
+            newContact.GetContactInfoFromUser();
+            Contact.AddContactToList(newContact);
 
-            newContact.FirstName = "Ritvik";
-            newContact.LastName = "Sharma";
-            newContact.Address = "123 Xyz";
-            newContact.City = "Banglore";
-            newContact.State = "Karnataka";
-            newContact.PostalCode = "12345";
-            newContact.PhoneNumber = "1234567890";
-            newContact.Email = "xyz@gmail.com";
-
-            Console.WriteLine("Contact Deatils :\n");
-            Console.WriteLine($"Name : {newContact.FirstName} {newContact.LastName}");
-            Console.WriteLine("Address : " + newContact.Address);
-            Console.WriteLine("City : " +newContact.City);
-            Console.WriteLine("State : " +newContact.State);
-            Console.WriteLine("PostalCode : " +newContact.PostalCode);
-            Console.WriteLine("PhoneNumber : " +newContact.PhoneNumber);
-            Console.WriteLine("Email : " + newContact.Email);
-
+            Contact.PrintContactList();
         }
     }
 }
